c11d140ac270d86c426bb23cf1fad5a24ce1dd00
sherm <shermawns@gmail.com>
2025-02-09 10:34:23 -0300

    feat: add migrations V3__create_tb_bank_account.sql

---
+++ src/main/resources/db/migration/V3__create_tb_bank_account.sql
@@ -0,0 +1,10 @@
+CREATE TABLE tb_bank_account (
+    id BIGSERIAL PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    account_number BIGINT UNIQUE NOT NULL,
+    agency VARCHAR(50) NOT NULL,
+    balance NUMERIC(15,2) NOT NULL DEFAULT 0.00,
+    active BOOLEAN NOT NULL DEFAULT TRUE,
+    account_type VARCHAR(50) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES tb_users(id) ON DELETE CASCADE
+);
